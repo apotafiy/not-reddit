c89c389746ecfbc79d020e816e23d3f00d730b67
Artem <apotafiy@uw.edu>
2021-10-17 19:11:18 -0700

    styled things

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,13 +1,59 @@
+import { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap';
+
 const Post = ({ dbData }) => {
   const { username, createdat, secureurl, upvotes } = dbData;
+
+  const [span, setSpan] = useState(null);
+
+  async function fetchData() {
+    const img = new Image();
+    img.addEventListener('load', function () {
+      const width = this.naturalWidth;
+      const height = this.naturalHeight;
+      const newSpan = Math.floor((height / width) * 24) + 24; // random formula that seems to work
+      setSpan(newSpan);
+    });
+    img.src = secureurl;
+  }
+  fetchData();
+
+  const outerStyles = {
+    margin: '15px 10px',
+    padding: '0',
+    gridRowEnd: `span ${span ? span : '26'}`,
+  };
+
   return (
-    <div className="post">
-      <img src={secureurl} alt="Dank Meme" />
-      <div className="post-info">
-        <p className="user-name">{`user: ${username}`}</p>
-        <p className="created-at">{`created at: ${createdat}`}</p>
-        <p className="karma">{`karma: ${upvotes}`}</p>
-      </div>
+    <div style={outerStyles} className="post">
+      <Card
+        style={{
+          boxShadow: '0.3rem 0.3rem 0.3rem rgb(0, 0, 0, 0.4)',
+          borderWidth: '1px',
+          borderColor: 'rgb(170,170,170)',
+          backgroundColor: 'rgba(var(--bs-dark-rgb))',
+        }}
+      >
+        <Card.Img variant="top" src={secureurl} />
+        <Card.Body style={{ color: 'white' }}>
+          <Card.Text>{`User: ${username}`}</Card.Text>
+          <Card.Text>{`Upload date: ${createdat}`}</Card.Text>
+          <Card.Text>{`Upvotes: ${upvotes}`}</Card.Text>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(secureurl, '_blank');
+            }}
+            variant="secondary"
+            style={{
+              backgroundColor: 'RGBA(255, 67, 0)',
+              borderColor: 'white',
+            }}
+          >
+            Full Image
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -2,68 +2,23 @@ import { useState, useEffect } from 'react';
 import Post from './Post';
 
 const PostList = () => {
-  // const db = [
-  //   {
-  //     postID: 1,
-  //     messageID: 1,
-  //     userID: 1,
-  //     publicID: 1,
-  //     version: 1,
-  //     format: 'png',
-  //     resourceType: 'image',
-  //     createdAt: 'time',
-  //     type: 'upload',
-  //     secureURL:
-  //       'https://res.cloudinary.com/dnuch8w9j/image/upload/v1626837602/c8sbdkpyspterqx1ppju.jpg',
-  //     upvotes: 1,
-  //   },
-  //   {
-  //     postID: 2,
-  //     messageID: 2,
-  //     userID: 2,
-  //     publicID: 2,
-  //     version: 2,
-  //     format: 'jpg',
-  //     resourceType: 'image',
-  //     createdAt: 'time',
-  //     type: 'upload',
-  //     secureURL:
-  //       'https://res.cloudinary.com/dnuch8w9j/image/upload/v1627599043/x7tghqkwbvox5hwopn00.png',
-  //     upvotes: 2,
-  //   },
-  //   {
-  //     postID: 3,
-  //     messageID: 3,
-  //     userID: 3,
-  //     publicID: 3,
-  //     version: 3,
-  //     format: 'webp',
-  //     resourceType: 'image',
-  //     createdAt: 'time',
-  //     type: 'upload',
-  //     secureURL:
-  //       'https://res.cloudinary.com/dnuch8w9j/image/upload/v1628193588/uz3m2izf0hwh6h2a3yve.png',
-  //     upvotes: 3,
-  //   },
-  // ];
   const [posts, setPosts] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      // TODO: when running the client the terminal said useEffect is sync so need to put everything in an async func inside for it to work
       const fetchedPosts = await fetch('/api/db/posts');
       const fetchedUsers = await fetch('/api/db/users');
       const postsData = await fetchedPosts.json();
-      postsData.sort((a, b) => a.createdat.localeCompare(b.createdat));
+      postsData.sort((a, b) => a.createdat.localeCompare(b.createdat)); // sort newest
       const usersData = await fetchedUsers.json();
-      const idNameMap = {};
+      const idNameMap = {}; // map userID and usernames
       usersData.forEach((user) => {
         if (idNameMap[user.userid] === undefined) {
           idNameMap[user.userid] = user.username;
         }
       });
 
-      postsData.splice(0, postsData.length - 10);
+      postsData.splice(0, postsData.length - 20);
       postsData.forEach((post) => {
         post.username = idNameMap[post.userid];
       });
@@ -74,8 +29,22 @@ const PostList = () => {
     fetchData();
   }, []);
 
+  const outerStyles = {
+    margin: '0',
+    padding: '0',
+    width: '85vw',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, 250px)',
+    gridAutoRows: '10px',
+    justifyContent: 'center',
+  };
+
   return (
-    <div className="post-list">
+    <div style={outerStyles} className="post-list">
+      <div>Sort by most popular, controversial, newest, oldest</div>
       {posts
         ? posts.map((post) => <Post dbData={post} key={post.postid} />)
         : 'Loading...'}
